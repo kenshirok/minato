d58f5d73fb6b403d8bcd0a96dd543ed6eba45e11
satake kenshiro <rule_0613_0522@yahoo.co.jp>
2021-05-12 11:32:40 +0900

    user- has-many-board

--- app/models/board.rb
+++ app/models/board.rb
@@ -3,10 +3,15 @@
 # Table name: boards
 #
 #  id         :integer          not null, primary key
-#  content    :text
-#  title      :string
+#  content    :text             not null
+#  title      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_boards_on_user_id  (user_id)
 #
 class Board < ApplicationRecord
   validates :title, presence: true
@@ -24,9 +29,15 @@ class Board < ApplicationRecord
     I18n.l(self.created_at, format: :default)
   end
 
+  def author_name
+    user.display_name
+  end
+
   private
   def validate_title_and_content_length
     char_count = self.title.length + self.content.length
     errors.add(:content, '10文字いじょうで！') unless char_count > 10
+
+    belongs_to :user
   end
 end
